02f98546f4504ae8d244eb6e81683a2925e1b1d8
igo <igoanggaraa@gmail.com>
2022-06-05 19:27:29 +0700

    challenge-callback

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (nameList, fnCallBack) => {
+  const tempArray = fnCallBack(nameList);
+
+  const arrData = [];
+  for (let i = 0; i < names.length; i++) {
+    arrData.push(i + 1 + ". " + names[i]);
+  }
+  return arrData;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+  return arr.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  return arr.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
